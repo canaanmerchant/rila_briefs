ad24489e7444b7096973c37c865cf20d6f7cbd77
Canaan Merchant <canaan.merchant@vexterra.com>
2020-08-03 09:43:58 -0400

    bringing heroku back up

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const briefRoutes = require('./routes/brief');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/admin', adminRoutes);
 app.use(briefRoutes);
 
